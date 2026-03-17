ce79907f7f82b3fc3158e685dbf1522bbfc1a413
dragonx943 <premieregirl26@gmail.com>
2026-03-18 01:58:21 +0700

    Fix build

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/TruyenTuoiTho.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/TruyenTuoiTho.kt
@@ -5,7 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("ZINMANGA", "Truyện Tuổi Thơ", "vi")
+@MangaSourceParser("TRUYENTUOITHO", "Truyện Tuổi Thơ", "vi")
 internal class TruyenTuoiTho(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.TRUYENTUOITHO, "truyentuoitho.com") {
 	override val datePattern = "dd/MM/yyyy"
